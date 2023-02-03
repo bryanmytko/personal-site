c301776b59fba1d9b1b5d0ad90c805892cf56e0c
Bryan Mytko <bryanmytko@gmail.com>
2023-02-02 21:45:56 -0800

    Add handling for asset requests

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::{
     fs,
-    io::{prelude::*, BufReader},
+    fs::{File},
+    io::{prelude::*, BufReader },
     net::{TcpListener, TcpStream},
 };
 
-static LISTEN_ADDR: &str = "127.0.0.1:3000";
+use chunked_transfer::Encoder;
+
+static LISTEN_ADDR: &str = "0.0.0.0:3000";
 
 fn main() {
     let listener = TcpListener::bind(LISTEN_ADDR).unwrap();
@@ -18,18 +21,48 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("index.html").unwrap();
-    let length = contents.len();
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("html/index.html").unwrap();
+        let length = contents.len();
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).unwrap();
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        let file_path = format!("assets/wordart.png");
+        let mut buf = Vec::new();
+        let mut file = File::open(&file_path).unwrap();
+        file.read_to_end(&mut buf).unwrap();
+        let mut encoded = Vec::new();
+
+        {
+            let mut encoder = Encoder::with_chunks_size(&mut encoded, 8);
+            encoder.write_all(&buf).unwrap();
+        }
+
+        let headers = [
+            "HTTP/1.1 200 OK",
+            "Content-type: image/jpeg",
+            "Transfer-Encoding: chunked",
+            "\r\n"
+        ];
+
+        let mut response = headers.join("\r\n")
+            .to_string()
+            .into_bytes();
+
+        response.extend(encoded);
+
+        stream.write_all(&response).unwrap();
+        match stream.write(&response) {
+            Ok(_) => println!("OK!"),
+            Err(_) => println!("Error: something went wrong!"),
+        }
+    }
+
+    // println!(">>> {}", request_line);
 }
