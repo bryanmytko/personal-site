7bd2b7465b252435da34ae80f15c6ff6073d09e5
Bryan Mytko <bryanmytko@gmail.com>
2023-02-03 01:17:30 -0800

    Handle GET requests for images; add random logo

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::{
     fs,
-    fs::{File},
     io::{prelude::*, BufReader },
     net::{TcpListener, TcpStream},
 };
 
-use chunked_transfer::Encoder;
+use rand::Rng;
+use regex::Regex;
 
 static LISTEN_ADDR: &str = "0.0.0.0:3000";
 
@@ -22,6 +22,7 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let image_regex = Regex::new(r"GET.*\.(png|gif|jpg|jpeg).*").unwrap();
 
     if request_line == "GET / HTTP/1.1" {
         let status_line = "HTTP/1.1 200 OK";
@@ -32,37 +33,43 @@ fn handle_connection(mut stream: TcpStream) {
             "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
         stream.write_all(response.as_bytes()).unwrap();
-    } else {
-        let file_path = format!("assets/wordart.png");
-        let mut buf = Vec::new();
-        let mut file = File::open(&file_path).unwrap();
-        file.read_to_end(&mut buf).unwrap();
-        let mut encoded = Vec::new();
+    } else if image_regex.is_match(&request_line) {
+        let logo = random_logo();
+        let file_path = format!("assets/{}", logo);
+        let contents = fs::read(file_path).unwrap();
 
-        {
-            let mut encoder = Encoder::with_chunks_size(&mut encoded, 8);
-            encoder.write_all(&buf).unwrap();
-        }
+        let status_line = "HTTP/1.1 200 OK";
 
-        let headers = [
-            "HTTP/1.1 200 OK",
-            "Content-type: image/jpeg",
-            "Transfer-Encoding: chunked",
-            "\r\n"
-        ];
+        let response = format!("{}\r\nContent-Length: {}\r\n\r\n",
+            status_line,
+            contents.len()
+        );
 
-        let mut response = headers.join("\r\n")
-            .to_string()
-            .into_bytes();
-
-        response.extend(encoded);
-
-        stream.write_all(&response).unwrap();
-        match stream.write(&response) {
-            Ok(_) => println!("OK!"),
-            Err(_) => println!("Error: something went wrong!"),
-        }
+        // @TODO
+        // let headers = [
+        //     "HTTP/1.1 200 OK",
+        //     "Content-type: image/jpeg",
+        //     "Transfer-Encoding: chunked",
+        //     "\r\n"
+        // ];
+        
+        stream.write(response.as_bytes()).unwrap();
+        stream.write(&contents).unwrap();
     }
+}
 
-    // println!(">>> {}", request_line);
+fn random_logo() -> &'static str {
+    let logos = [
+        "wordart1.png",
+        "wordart2.png",
+        "wordart3.png",
+        "wordart4.png",
+        "wordart5.png",
+        "wordart.gif",
+    ];
+
+    let mut rng = rand::thread_rng();
+    let rng_index = rng.gen_range(1..logos.len());
+
+    return logos[rng_index];
 }
